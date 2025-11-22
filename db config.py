a739c41ebde4bced2070221089660417e0e8d770
eshuuux <ashishkhoteak47@gmail.com>
2025-11-22 13:27:22 +0530

    Update

--- db config.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-con=sql.connect("Jobmatch.db")
-c=con.cursor()
-c.execute('''INSERT INTO jobs (title, company, location, skills_required, experience, description)
-VALUES ("Python Developer", "TCS", "Pune", "python, flask, sql", "0-1 Year", "Looking for Python fresher with Flask experience.")''')
-con.commit()
-con.close()
